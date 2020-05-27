a1f863a58f0b625aae6f7498cb687ac134eee59a
juniormargalho <juniormargalho@gmail.com>
2020-05-27 14:37:07 -0300

    interfaceAutenticacao

--- app/src/main/java/com/juniormargalho/uber/activity/MapsActivity.java
+++ app/src/main/java/com/juniormargalho/uber/activity/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.juniormargalho.uber;
+package com.juniormargalho.uber.activity;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -10,8 +10,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.juniormargalho.uber.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
     private GoogleMap mMap;
 
     @Override
